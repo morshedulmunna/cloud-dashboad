1a0c52ea91adab4074ec269ccb42368b2bbde265
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-23 14:52:42 +0600

    quick access

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import "./src/styles/style.scss";
+
+document.addEventListener("DOMContentLoaded", function () {
+  var scrollContainer = document.querySelector(".card_area_wrapper");
+  scrollContainer.addEventListener("wheel", function (event) {
+    event.preventDefault();
+    var scrollLeft = scrollContainer.scrollLeft;
+    scrollContainer.scrollLeft = scrollLeft + event.deltaY;
+  });
+});
