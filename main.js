de30363e5687ddac46985dc83257e439f14d0ffc
morshedulmunna <morshedulmunna1@gmail.com>
2023-08-23 16:53:11 +0600

    done

--- main.js
+++ main.js
@@ -8,3 +8,40 @@ document.addEventListener("DOMContentLoaded", function () {
     scrollContainer.scrollLeft = scrollLeft + event.deltaY;
   });
 });
+
+// chart
+const ctx = document.getElementById("myChart");
+
+new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [
+      "JAN",
+      "FEB",
+      "MAR",
+      "APR",
+      "MAY",
+      "JUN",
+      "JULY",
+      "AUG",
+      "SEP",
+      "NOV",
+      "DEC",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 5, 2, 4, 2, 9, 3, 5, 2, 3, 4, 4, 3, 2, 5],
+        drawTicks: false,
+        backgroundColor: "#25396f",
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
